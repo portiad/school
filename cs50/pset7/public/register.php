2693ec993e360576f5bc25f9d1c4b34f70657307
portiad <portiad@gmail.com>
2014-09-18 15:45:59 -0700

    updates

--- cs50/pset7/public/register.php
+++ cs50/pset7/public/register.php
@@ -7,8 +7,21 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
         $result = query("INSERT INTO users (username, hash, cash) VALUES(?, ?, 10000.00)", $_POST["username"], crypt($_POST["password"]));
-        $rows = query("SELECT LAST_INSERT_ID() AS id");
-        $id = $rows[0]["id"];
+        
+        if ($result === true)
+        {
+            apologize("Username is taken. Choose another one. ");
+        }
+        else
+        {
+            $rows = query("SELECT LAST_INSERT_ID() AS id");
+            $id = $rows[0]["id"];
+            
+            $_SESSION["id"] = $row["id"];
+
+            redirect("/");
+
+        }
     }
     else
     {
--- cs50/pset7/templates/register_form.php
+++ cs50/pset7/templates/register_form.php
@@ -1,4 +1,4 @@
-<form action="login.php" method="post">
+<form action="register.php" method="post">
     <fieldset>
         <div class="form-group">
             <input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
@@ -7,7 +7,7 @@
             <input class="form-control" name="password" placeholder="Password" type="password"/>
         </div>
         <div class="form-group">
-            <button type="submit" class="btn btn-default">Log In</button>
+            <button type="submit" class="btn btn-default">Register</button>
         </div>
     </fieldset>
 </form>

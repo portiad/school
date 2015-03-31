f021e63d29d838e6e7df0045d5e15b343eadb352
portia <portiad@gmail.com>
2015-03-31 11:51:41 -0700

    tableview

--- ios/ios8/TaskIt/TaskIt/ViewController.swift
+++ ios/ios8/TaskIt/TaskIt/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
+        cell.taskLabel.text = "Study French"
+        cell.descriptionLabel.text = "Verbs in past and present"
+        cell.dateLabel.text = "01/10/2014"
+        
+        
+        return cell
+    }
+    
+    // UITableViewDelegate
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
 

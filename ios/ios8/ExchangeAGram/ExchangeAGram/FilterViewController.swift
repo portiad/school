42a4adb1d7963ae0cd57b861721efb4c7d628e80
portia <portiad@gmail.com>
2015-04-17 11:31:44 -0700

    filter collection view

--- ios/ios8/ExchangeAGram/ExchangeAGram/FilterViewController.swift
+++ ios/ios8/ExchangeAGram/ExchangeAGram/FilterViewController.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     var thisFeedItem: FeedItem!
+    
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let layout = UICollectionViewFlowLayout()
+        // boarders & sizes
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 150.0, height: 150.0)
+        //initialize the instance
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +37,12 @@ class FilterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
     }
-    */
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
 
 }

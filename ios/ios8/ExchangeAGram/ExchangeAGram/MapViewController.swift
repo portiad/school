f081accc547f92166d4564af5c3ad380d616bafa
portia <portiad@gmail.com>
2015-04-20 15:40:21 -0700

    map paris

--- ios/ios8/ExchangeAGram/ExchangeAGram/MapViewController.swift
+++ ios/ios8/ExchangeAGram/ExchangeAGram/MapViewController.swift
@@ -17,15 +17,25 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
         //Paris
-        var location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
-        //amount of area span for the map
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        //region map to display
-        let region = MKCoordinateRegionMake(location, span)
-        mapView.setRegion(region, animated: true)
-        
-        
+//        var location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
+//        //amount of area span for the map
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+//        //region map to display
+//        let region = MKCoordinateRegionMake(location, span)
+//        mapView.setRegion(region, animated: true)
+//        
+//        //setting up marker
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = location
+//        annotation.title = "Canal Saint-Martin"
+//        annotation.subtitle = "Paris"
+//        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {

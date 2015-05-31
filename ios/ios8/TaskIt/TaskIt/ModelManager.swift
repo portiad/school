2513975b5f31268ad2e7f4099b0d1b9c72284ce0
portia <portiad@gmail.com>
2015-05-31 12:21:43 -0700

    added model manager

--- ios/ios8/TaskIt/TaskIt/ModelManager.swift
+++ ios/ios8/TaskIt/TaskIt/ModelManager.swift
@@ -34,7 +34,7 @@ class ModelManager: NSObject {
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "com.dprados.CoreDataSpike" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1] as NSURL
+        return urls[urls.count-1] as! NSURL
         }()
     
     lazy var managedObjectModel: NSManagedObjectModel = {
@@ -61,14 +61,14 @@ class ModelManager: NSObject {
         var failureReason = "There was an error creating or loading the application's saved data."
         var options: NSDictionary = [NSMigratePersistentStoresAutomaticallyOption : NSNumber(bool: true), NSInferMappingModelAutomaticallyOption : NSNumber(bool: true),
             NSPersistentStoreUbiquitousContentNameKey : "TaskItCloud"]
-        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options, error: &error) == nil {
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options as [NSObject : AnyObject], error: &error) == nil {
             coordinator = nil
             // Report any error we got.
             let dict = NSMutableDictionary()
             dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
             dict[NSLocalizedFailureReasonErrorKey] = failureReason
             dict[NSUnderlyingErrorKey] = error
-            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict as [NSObject : AnyObject])
             // Replace this with code to handle the error appropriately.
             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog("Unresolved error \(error), \(error!.userInfo)")
@@ -128,9 +128,9 @@ class ModelManager: NSObject {
             moc.mergeChangesFromContextDidSaveNotification(notification)
             var changes: NSDictionary = notification.userInfo!
             var allChanges: NSMutableSet = NSMutableSet()
-            allChanges.unionSet(changes.valueForKey(NSInsertedObjectsKey) as NSSet)
-            allChanges.unionSet(changes.valueForKey(NSUpdatedObjectsKey) as NSSet)
-            allChanges.unionSet(changes.valueForKey(NSDeletedObjectsKey) as NSSet)
+            allChanges.unionSet(changes.valueForKey(NSInsertedObjectsKey) as! Set<NSObject>)
+            allChanges.unionSet(changes.valueForKey(NSUpdatedObjectsKey) as! Set<NSObject>)
+            allChanges.unionSet(changes.valueForKey(NSDeletedObjectsKey) as! Set<NSObject>)
         }
         NSNotificationCenter.defaultCenter().postNotificationName(kCoreDataUpdated, object: self)
     }

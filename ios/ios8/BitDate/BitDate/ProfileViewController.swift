9a56c7ddbf835a8417b690002d264ea4a92e5bf2
portia <portiad@gmail.com>
2015-06-09 11:43:27 -0700

    profileViewController

---
+++ ios/ios8/BitDate/BitDate/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  BitDate
+//
+//  Created by Portia Dean on 6/9/15.
+//  Copyright (c) 2015 Portia Dean. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
